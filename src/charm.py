7d9a5f0aa1f74e3ad6c450cb34ae995298d0e544
Jean-Pierre <jeanpierre.roquesalane@dell.com>
2024-03-08 14:57:01 +0000

    initial version of charm

--- src/charm.py
+++ src/charm.py
@@ -15,41 +15,32 @@
 # limitations under the License.
 
 
-import uuid
-import logging
+import charms_openstack.charm
 
-from ops.main import main
-from ops_openstack.plugins.classes import CinderStoragePluginCharm
-
-# Log messages can be retrieved using juju debug-log
-logger = logging.getLogger(__name__)
+charms_openstack.charm.use_defaults('charm.default-select-release')
 
 VOLUME_DRIVER = 'cinder.volume.drivers.dell_emc.powerflex.driver.PowerFlexDriver'
 
 
-class CinderPowerflexCharm(CinderStoragePluginCharm):
-    
+class CinderPowerflexCharm(
+    charms_openstack.charm.CinderStoragePluginCharm):
+        
     PACKAGES = ['cinder-common']
     # Overriden from the parent. May be set depending on the charm's properties
-    stateless = False
-    active_active = False
+    stateless = True
+    
 
     mandatory_config = [
         'san-ip', 'san-login', 'san-password'
     ]
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self._stored.is_started = True
-
     def cinder_configuration(self, charm_config) -> 'list[tuple]':
         """Return the configuration to be set by cinder"""
         cget = charm_config.get
 
-        volume_backend_name = cget(
-            'volume-backend-name') or self.framework.model.app.name
+        volume_backend_name = cget('volume-backend-name')
 
-        raw_options = [
+        driver_options = [
             ('volume_driver', VOLUME_DRIVER),
             ('volume_backend_name', volume_backend_name),
             ('san_ip', cget('san-ip')),
@@ -69,8 +60,5 @@ class CinderPowerflexCharm(CinderStoragePluginCharm):
             ('rest_api_connect_timeout', cget('rest-api-connect-timeout')),
             ('rest_api_read_timeout', cget('rest-api-read-timeout'))
         ]
-        options = [(x, y) for x, y in raw_options if y]
-        return options
-
-if __name__ == "__main__": 
-    main(CinderPowerflexCharm)
+        final_options = driver_options
+        return final_options
