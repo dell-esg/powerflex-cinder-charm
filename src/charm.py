45aeecec210eb70d8f187edf4eb6c6bf54271c7e
Billy Olsen <billy.olsen@canonical.com>
2024-06-10 21:24:27 -0700

    Add unit tests
    Add unit tests and update requirements for running the unit tests.
    
    Signed-off-by: Billy Olsen <billy.olsen@canonical.com>

--- src/charm.py
+++ src/charm.py
@@ -16,11 +16,12 @@
 
 """Charmed operator for Dell PowerFlex Cinder driver."""
 
+import logging
 import os
 import subprocess
+from typing import Iterable, Union
 
 import charmhelpers.core as ch_core
-from charmhelpers.core.hookenv import ERROR, log
 from charmhelpers.core.host import service_running
 from charmhelpers.core.templating import render
 from ops import model
@@ -31,29 +32,55 @@ VOLUME_DRIVER = "cinder.volume.drivers.dell_emc.powerflex.driver.PowerFlexDriver
 CONNECTOR_DIR = "/opt/emc/scaleio/openstack"
 CONNECTOR_FILE = "connector.conf"
 
+logger = logging.getLogger(__name__)
+
 
 class CinderPowerflexCharm(CinderStoragePluginCharm):
     """Cinder subordinate charm for Dell PowerFlex drivers."""
 
     PACKAGES = ["cinder-common"]
-    # Overridden from the parent. May be set depending on the charm's properties
-    stateless = True
-    # Active/Active configuration is not supported at this time
-    active_actibe = False
 
-    mandatory_config = ["san-ip", "san-login", "san-password"]
+    # Defines the required relations/integrations that must be connected
+    # in order for this charm to function correctly. The charm will go into
+    # the blocked status if these relations are not present.
+    REQUIRED_RELATIONS = ["storage-backend"]
+
+    # Defines the mandatory configuration values that need to be specified
+    # by the user in order for this charm to function correctly.
+    MANDATORY_CONFIG = [
+        "powerflexgw-ip",
+        "powerflexgw-login",
+        "powerflexgw-password",
+    ]
+
+    # Restart map is used to map which services should be restarted
+    # when the specified file is changed on disk. The key is the file,
+    # the value is a list of services which need to be restarted.
+    RESTART_MAP = {
+        "/opt/emc/scaleio/openstack/connector.conf": ["scini"],
+    }
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self._stored.is_started = True
 
-    def cinder_configuration(self, charm_config) -> "list[tuple]":
+    @property
+    def stateless(self):
+        """Indicate whether the cinder driver provides a stateless cinder backend."""
+        return True
+
+    @property
+    def active_active(self):
+        """Indicate whether the cinder driver supports an active/active configuration."""
+        # Active/Active configuration is not supported at this time
+        return False
+
+    def cinder_configuration(self, charm_config) -> Iterable[tuple[str, Union[str, int, bool]]]:
         """Return the configuration to be set by Cinder."""
         cget = charm_config.get
 
         raw_options = [
             ("volume_driver", VOLUME_DRIVER),
-            ("volume_backend_name", cget("volume-backend-name")),
+            ("volume_backend_name", cget("volume-backend-name", self.app.name)),
             ("san_ip", cget("powerflexgw-ip")),
             ("san_login", cget("powerflexgw-login")),
             ("san_password", cget("powerflexgw-password")),
@@ -75,10 +102,11 @@ class CinderPowerflexCharm(CinderStoragePluginCharm):
             ("replication_device", cget("powerflex-replication-config")),
         ]
 
-        options = [(x, y) for x, y in raw_options if y]
+        options = [(x, y) for x, y in raw_options if y is not None or ""]
         return options
 
-    def _on_install(self, event):
+    def on_install(self, event):
+        """Handle install event by rendering config files and installing packages."""
         super().on_install(event)
         self.create_connector()
         self.install_sdc()
@@ -88,9 +116,8 @@ class CinderPowerflexCharm(CinderStoragePluginCharm):
         """Create the connector.conf file and populate with data."""
         config = dict(self.framework.model.config)
         powerflex_backend = dict(self.cinder_configuration(config))
-        powerflex_config = {}
+        powerflex_config = {"cinder_name": self.framework.model.app.name}
         # Get cinder config stanza name.
-        powerflex_config["cinder_name"] = self.framework.model.app.name
         filename = os.path.join(CONNECTOR_DIR, CONNECTOR_FILE)
         ch_core.host.mkdir(CONNECTOR_DIR)
 
@@ -113,7 +140,7 @@ class CinderPowerflexCharm(CinderStoragePluginCharm):
 
         # Render the templates/connector.conf and create the
         # /opt/emc/scaleio/openstack/connector.conf with root access only
-        log("Rendering connector.conf template with config {}".format(powerflex_config))
+        logger.debug("Rendering connector.conf template with config {}".format(powerflex_config))
         render(
             source="connector.conf",
             target=filename,
@@ -123,32 +150,33 @@ class CinderPowerflexCharm(CinderStoragePluginCharm):
 
     def install_sdc(self):
         """Install the SDC debian package in order to get access to the PowerFlex volumes."""
-        config = dict(self.framework.model.config)
         sdc_package_file = self.model.resources.fetch("sdc-deb-package")
         # Check if the file exists
         if os.path.isfile(sdc_package_file):
             # Get the MDM IP from config file
-            sdc_mdm_ips = config["powerflex-sdc-mdm-ips"]
+            sdc_mdm_ips = self.model.config["powerflex-sdc-mdm-ips"]
             # Install the SDC package
             install_cmd = f"sudo MDM_IP={sdc_mdm_ips} dpkg -i {sdc_package_file}"
-            log("Installing SDC kernel module with MDM(s) {}".format(sdc_mdm_ips))
+            logger.info("Installing SDC kernel module with MDM(s) {}".format(sdc_mdm_ips))
             result = subprocess.run(install_cmd.split(), capture_output=True, text=True)
             exit_code = result.returncode
             if exit_code != 0:
-                log(
-                    "An error occurred during the SDC installation: {}.".format(result.stderr),
-                    level=ERROR,
+                logger.error(
+                    "An error occurred during the SDC " "installation: {}.".format(result.stderr)
                 )
             else:
-                log("SDC installed successfully, stdout: {}".format(result.stdout))
+                logger.info("SDC installed successfully, stdout: {}".format(result.stdout))
                 # Check if service scini is running
                 if service_running("scini"):
-                    log("SDC scini service running. SDC Installation complete.")
+                    logger.info("SDC scini service running. SDC Installation complete.")
+                    # Make sure to mark the state as started.
+                    self._stored.is_started = True
                 else:
-                    log("SDC scini service has encountered errors while starting", level="ERROR")
+                    logger.error("SDC scini service has encountered errors while starting")
+                    self._stored.is_started = False
         else:
-            log("The package required for SDC installation is missing.", level="ERROR")
-            model.BlockedStatus("SDC package missing")
+            logger.error("The package required for SDC installation is missing.")
+            self.unit.status = model.BlockedStatus("SDC package missing")
 
 
 if __name__ == "__main__":
